8ade8a2dbff862e68fc44da119e06b1864287e3d
Katrina Renarde <katthefoxmaid@gmail.com>
2023-12-30 23:11:16 -0800

    fix: logger newline

--- src/features/technical/logger.ts
+++ src/features/technical/logger.ts
@@ -3,7 +3,7 @@ import { config, persistentDataHandler } from "./persistentDataHandler";
 
 export async function log(text: string): Promise<void> {
   if (!persistentDataHandler.initialised) await persistentDataHandler.init();
-  console.log(text);
+  console.log(`\n${text}`);
   writeFileSync(config.logFile, text, { flag: "a" });
 }
 export function logError(error: Error): void {
